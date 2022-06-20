2421cc0166faf3eefeb53e6505f8e547e4fdf316
dymafr <erwan1309@hotmail.com>
2022-06-20 16:58:28 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-
-function handleClick(e, from) {
-  console.log('CLIC', e);
-  console.log('CLIC', from);
-}
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-20">
-      <button onClick={(e) => handleClick(e, 'button')} className="mb-20">
-        Submit
+      <button onClick={handleClick} className="mb-20">
+        Submit {count}
       </button>
     </div>
   );
